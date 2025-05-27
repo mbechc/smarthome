7198fb73cc20445b5ef43ca50b8f09946d628873
Morten Bech Christensen <morten_b_christensen@mac.com>
2025-05-27 22:09:04 +0200

    Add files via upload

---
+++ doers/spisebordGroup.js
@@ -0,0 +1,10 @@
+module.exports = {
+  id: 'spisebordGroup',
+  type: 'http',
+  method: 'put',
+  address: 'http://192.168.88.157/api/n7tQojvnCYgoUeFsAhHQ0S0q18zZZqjmIoHRoeef/groups/1/action',
+  payload: {
+    on: true,
+    bri: 200
+  }
+};
